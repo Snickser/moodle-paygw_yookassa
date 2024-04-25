7dd05e9b98238eefaf614288dd2edcd7ecce79cd
Snickser <snickser@gmail.com>
2024-04-25 20:14:16 +0300

    add

--- callback.php
+++ callback.php
@@ -35,7 +35,7 @@ $data = json_decode($source, false);
 $orderid  = $data->object->id;
 $outsumm  = $data->object->amount->value;
 
-file_put_contents("/tmp/yyyyy", serialize($data)."\n", FILE_APPEND);
+// file_put_contents("/tmp/yyyyy", serialize($data) . "\n", FILE_APPEND);
 
 if ($data->event !== 'payment.succeeded') {
     die('FAIL. Payment not successed');
@@ -59,43 +59,41 @@ $userid      = $yookassatx->userid;
 $config = (object) helper::get_gateway_configuration($component, $paymentarea, $itemid, 'yookassa');
 $payable = helper::get_payable($component, $paymentarea, $itemid);
 
-    // Check that amount paid is the correct amount
-    if ((float) $yookassatx->cost <= 0) {
-        $cost = (float) $payable->get_amount();
-    } else {
-        $cost = (float) $yookassatx->cost;
+// Check that amount paid is the correct amount
+if ((float) $yookassatx->cost <= 0) {
+    $cost = (float) $payable->get_amount();
+} else {
+    $cost = (float) $yookassatx->cost;
+}
+
+// Use the same rounding of floats as on the paygw form.
+$cost = number_format($cost, 2, '.', '');
+$outsumm = number_format($outsumm, 2, '.', '');
+
+if ($yookassatx->currency == 'RUB') {
+    if ($outsumm !== $cost) {
+        die('FAIL. Amount does not match.');
     }
-    // Use the same rounding of floats as on the paygw form.
-    $cost = number_format($cost, 2, '.', '');
-    $outsumm = number_format($outsumm, 2, '.', '');
+}
 
-    if ($yookassatx->currency == 'RUB') {
-        if ($outsumm !== $cost) {
-            die('FAIL. Amount does not match.');
-        }
-    }
-    // Deliver course
-    // $fee = helper::get_rounded_cost($payable->get_amount(), $payable->get_currency(), helper::get_gateway_surcharge('yookassa'));
-    $paymentid = helper::save_payment(
-        $payable->get_account_id(),
-        $component,
-        $paymentarea,
-        $itemid,
-        $userid,
-        $cost,
-        $payable->get_currency(),
-        'yookassa'
-    );
-    helper::deliver_order($component, $paymentarea, $itemid, $paymentid, $userid);
+// Deliver course
+// $fee = helper::get_rounded_cost($payable->get_amount(), $payable->get_currency(), helper::get_gateway_surcharge('yookassa'));
+$paymentid = helper::save_payment(
+    $payable->get_account_id(),
+    $component,
+    $paymentarea,
+    $itemid,
+    $userid,
+    $cost,
+    $payable->get_currency(),
+    'yookassa'
+);
+helper::deliver_order($component, $paymentarea, $itemid, $paymentid, $userid);
 
-
-
-
-    // Write to DB
-    $yookassatx->success = 1;
-    if (!$DB->update_record('paygw_yookassa', $yookassatx)) {
-        die('FAIL. Update db error.');
-    } else {
-        die("OK");
-    }
-
+// Write to DB
+$yookassatx->success = 1;
+if (!$DB->update_record('paygw_yookassa', $yookassatx)) {
+    die('FAIL. Update db error.');
+} else {
+    die("OK");
+}
