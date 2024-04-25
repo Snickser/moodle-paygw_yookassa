a72c4a972888cddae055f898961e4e1a96ea483e
Snickser <snickser@gmail.com>
2024-04-25 17:12:50 +0300

    add

--- method.php
+++ method.php
@@ -25,8 +25,7 @@
 
 use core_payment\helper;
 
-require_once(__DIR__ . '/yookassa-sdk-php/lib/autoload.php');
-use YooKassa\Client;
+require_once(__DIR__ . '/yookassa-sdk-php/vendor/autoload.php');
 
 require_once(__DIR__ . '/../../../config.php');
 global $CFG, $USER, $DB;
