7a83dba62e852ee39edf04e80908316d20b7039c
root <snickser@gmail.com>
2024-05-10 09:01:50 +0300

    tune

--- pay.php
+++ pay.php
@@ -33,17 +33,14 @@ global $CFG, $USER, $DB;
 
 $userid = $USER->id;
 
-$component   = required_param('component', PARAM_ALPHANUMEXT);
-$paymentarea = required_param('paymentarea', PARAM_ALPHANUMEXT);
+$component   = required_param('component', PARAM_COMPONENT);
+$paymentarea = required_param('paymentarea', PARAM_AREA);
 $itemid      = required_param('itemid', PARAM_INT);
 $description = required_param('description', PARAM_TEXT);
 
 $password    = optional_param('password', null, PARAM_TEXT);
 $skipmode    = optional_param('skipmode', 0, PARAM_INT);
 $costself    = optional_param('costself', null, PARAM_TEXT);
-
-$description = json_decode("\"$description\"");
-
 
 $config = (object) helper::get_gateway_configuration($component, $paymentarea, $itemid, 'yookassa');
 $payable = helper::get_payable($component, $paymentarea, $itemid);// Get currency and payment amount.
