ae8f1826f9772f36a78da1c3bef43a10e7a2fbc6
Snickser <snickser@gmail.com>
2024-05-20 08:46:14 +0300

    add

--- method.php
+++ method.php
@@ -39,7 +39,7 @@ $paymentarea = required_param('paymentarea', PARAM_AREA);
 $itemid      = required_param('itemid', PARAM_INT);
 $description = required_param('description', PARAM_TEXT);
 
-$description = json_decode("\"$description\"");
+$description = json_decode('"' . $description . '"');
 
 $params = [
     'component'   => $component,
