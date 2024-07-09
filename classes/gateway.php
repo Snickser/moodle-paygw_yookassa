24edf7ebbc21c65431e935e7ed8a469224c08e8c
Snickser <snickser@gmail.com>
2024-07-09 16:04:37 +0300

    add

--- classes/gateway.php
+++ classes/gateway.php
@@ -197,7 +197,8 @@ class gateway extends \core_payment\gateway {
         $mform->addElement('html', get_string('callback_help', 'paygw_yookassa') . '</div><br>');
 
         $header = '<div>Новые версии плагина вы можете найти на
- <a href=https://github.com/Snickser/moodle-paygw_yookassa>https://github.com/Snickser/moodle-paygw_yookassa</a></div><br>';
+ <a href=https://github.com/Snickser/moodle-paygw_yookassa>https://github.com</a><br>
+ Пожалуйста, отправьте мне немножко <a href="https://www.paypal.com/paypalme/snickser">доната</a>😊</div>';
         $mform->addElement('html', $header);
     }
 
