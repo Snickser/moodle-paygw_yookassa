f487b381e380bf6808bef5ceacd8bce4d55b2892
Snickser <snickser@gmail.com>
2024-07-09 15:18:34 +0300

    add

--- lang/ru/paygw_yookassa.php
+++ lang/ru/paygw_yookassa.php
@@ -79,7 +79,7 @@ $string['wallet'] = 'ЮMoney кошелёк';
 $string['plastic'] = 'VISA, MasterCard, МИР';
 $string['sbp'] = 'СБП (QR-код)';
 
-$string['messagesubject'] = 'Уведомление о платеже';
+$string['messagesubject'] = 'Уведомление о платеже ({$a})';
 
 $string['message_success_completed'] = 'Здравствуйте, {$a->firstname}!
 Ваша платёжная транзакция № {$a->orderid} на {$a->localizedcost} успешно завершена. Спасибо за ваше пожертвование.
