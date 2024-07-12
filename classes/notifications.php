1f94b309180b789393de69ed2ddc1e35c594e13e
Snickser <snickser@gmail.com>
2024-07-12 09:47:10 +0300

    fix

--- classes/notifications.php
+++ classes/notifications.php
@@ -61,6 +61,7 @@ class notifications {
             'orderid'   => $orderid,
             'firstname' => $user->firstname,
             'fullname'  => fullname($user),
+            'url'       => $CFG->wwwroot,
             'localizedcost' => \core_payment\helper::get_cost_as_string($fee, $currency),
         ];
 
@@ -70,7 +71,6 @@ class notifications {
         $message->userfrom  = \core_user::get_noreply_user(); // If the message is 'from' a specific user you can set them here.
         $message->userto    = $user;
         $message->subject   = get_string('messagesubject', 'paygw_yookassa', $type);
-        $message->url       = $CFG->wwwroot;
         switch ($type) {
             case 'Success completed':
                 $messagebody = get_string('message_success_completed', 'paygw_yookassa', $a);
