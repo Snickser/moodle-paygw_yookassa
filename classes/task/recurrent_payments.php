36a26c393f8fefa411baab8b8c08e4c36fd4979e
Snickser <snickser@gmail.com>
2024-07-19 09:43:45 +0300

    add

--- classes/task/recurrent_payments.php
+++ classes/task/recurrent_payments.php
@@ -56,8 +56,8 @@ class recurrent_payments extends \core\task\scheduled_task {
         mtrace('Start');
 
         // Stage One.
-        $stime = strtotime("+1day");
-        $ctime = strtotime("+1day1hour");
+        $stime = strtotime(date('d-M-Y H:00', strtotime("+1day")));
+        $ctime = strtotime(date('d-M-Y H:00', strtotime("+1day1hour")));
 
         $yookassatx = $DB->get_records_sql('SELECT * FROM {paygw_yookassa} WHERE (success=1 OR success=3) ' .
                   'AND recurrent>? AND recurrent<?', [ $stime, $ctime ]);
