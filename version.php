87b03e2e11ec699615207c0ccfbe1d4570f7a34e
Snickser <snickser@gmail.com>
2024-09-04 08:30:01 +0300

    version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024071901;
+$plugin->version   = 2024090400;
 $plugin->requires  = 2023100900;
 $plugin->component = 'paygw_yookassa';
 $plugin->release   = '2.2';
