4f7af219a7cd98001ba2c6f869f773d2b72ca014
Snickser <snickser@gmail.com>
2025-03-04 11:01:03 +0300

    add

--- method.php
+++ method.php
@@ -55,9 +55,10 @@ $surcharge = helper::get_gateway_surcharge('yookassa');// In case user uses surc
 $fee = helper::get_rounded_cost($payable->get_amount(), $currency, $surcharge);
 
 // Get course info.
-$enrolperiod = '';
-$enrolperioddesc = '';
+$enrolperiod = 0;
+$enrolperioddesc = null;
 $uninterrupted = false;
+$enrolperiod = true;
 if ($component == "enrol_yafee") {
     $cs = $DB->get_record('enrol', ['id' => $itemid, 'enrol' => 'yafee']);
     $enrolperiod = $cs->enrolperiod;
@@ -91,12 +92,23 @@ if ($component == "enrol_yafee") {
             }
         }
     }
+    // Set month/year period.
+    if ($cs->customchar1 == 'month' && $cs->customint7 > 0) {
+        $enrolperiod = $cs->customint7;
+        $enrolperioddesc = get_string('months');
+        $showenrolperiod = false;
+    } else if ($cs->customchar1 == 'year' && $cs->customint7 > 0) {
+        $enrolperiod = $cs->customint7;
+        $enrolperioddesc = get_string('years');
+        $showenrolperiod = false;
+    }
 } else if ($component == "mod_gwpayments") {
     $cs = $DB->get_record('gwpayments', ['id' => $itemid]);
     $enrolperiod = $cs->costduration;
 }
 
-if ($enrolperiod > 0) {
+// Set standard periods.
+if ($enrolperiod > 0 && $enrolperiod) {
     if ($enrolperiod >= 86400 * 7) {
         $enrolperioddesc = get_string('weeks');
         $enrolperiod = round($enrolperiod / (86400 * 7));
